b41e630120d16a8856172704e49c43188abf3032
Guillaume <guillaumedelvaux@hotmail.com>
2021-02-21 22:20:12 +0200

    day2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [height,setHeight] = useState(185);
+  const [age,setAge] = useState(30);
+  const [name,setName] = useState("Guillaume");
+  
+  const ageUp = () => {
+    setAge(age+1);
+  }
+  const heightUp = () => {
+    setHeight (height+1);
+  }
+  const changeName = (event) => {
+    setName(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="child1">
+        <span className="text">{name}</span>
+        <input value={name} onChange={changeName} />
+      </div>
+
+      <div className="child2" onClick={heightUp}>
+        <span className="text">{height}cm</span>
+      </div>
+
+      <div className="child3" onClick={ageUp}>
+        <span className="text">{age}</span>
+      </div>
+
+    </div >
   );
 }
 
